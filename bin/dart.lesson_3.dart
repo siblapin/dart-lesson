cfcfeea8defa541e4db84427fc8cf86a3299aecb
siblapin <sib.lapin@mail.ru>
2022-07-07 23:45:58 +0700

    Lesson3

--- bin/dart.lesson_3.dart
+++ bin/dart.lesson_3.dart
@@ -1,3 +1,21 @@
+import 'dart:typed_data';
+
 void main() {
-  print('работает');
+// задание 3
+//Реализуйте метод, который принимает строку слов, разделённых пробелами. Задача — найти в данной строке числа и вернуть коллекцию num этих чисел.
+
+  var Name = "Aleks";
+  var age = "34";
+  var home = "Sib";
+  var Person = '$Name $age $home';
+  print('Запрос: $Person');
+
+  findNumsIn(String string) {
+    RegExp exp = RegExp(r"[^0-9]+");
+    String str = string;
+    String res = str.replaceAll(exp, '');
+    print('Результат: $res');
+  }
+
+  var Person1 = findNumsIn(Person);
 }
