ead86b3272ead7e11bdcbf3b51b06a1423920c39
siblapin <sib.lapin@mail.ru>
2022-07-12 20:19:43 +0700

    lesson4.2

--- bin/dart.lesson_3.dart
+++ bin/dart.lesson_3.dart
@@ -4,11 +4,11 @@ void main() {
 // задание 3
 //Реализуйте метод, который принимает строку слов, разделённых пробелами. Задача — найти в данной строке числа и вернуть коллекцию num этих чисел.
 
-  var Name = "Aleks";
+  var name = "Aleks";
   var age = "34";
   var home = "Sib";
-  var Person = '$Name $age $home';
-  print('Запрос: $Person');
+  var person = '$name $age $home';
+  print('Запрос: $person');
 
   findNumsIn(String string) {
     RegExp exp = RegExp(r"[^0-9]+");
@@ -17,5 +17,5 @@ void main() {
     print('Результат: $res');
   }
 
-  var Person1 = findNumsIn(Person);
+  var Person1 = findNumsIn(person);
 }
--- bin/dart_lesson_4.dart
+++ bin/dart_lesson_4.dart
@@ -1,12 +1,17 @@
 //Есть коллекция слов. Реализуйте метод, возвращающий Map. Данный Map должен соотносить слово и количество его вхождений в данную коллекцию.
 void main() {
-  final List<String> NamePerson = ["Alex", "Petr", "Evgen", "Alex"];
-  var a = "Alex";
-  var Rezalt = <String, int>{};
-
-  for (var i = 0; i < 9; i++) {
-    if (NamePerson.isEmpty == a) {
-      print('один');
+  final Map<int, String> NamePerson = {
+    1: "Alex",
+    2: "Petr",
+    3: "Evgen",
+    4: "Petr"
+  };
+  var name = "Alex";
+  var sum = 0;
+  for (var index = 1; index < 5; index++) {
+    if (NamePerson[index] == name) {
+      sum = sum + 1;
     }
   }
+  print(sum);
 }
