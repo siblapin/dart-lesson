758321c363d3dab17ebf37d96aaaa824bda007de
siblapin <sib.lapin@mail.ru>
2022-07-12 17:54:43 +0700

    Lesson4.1

--- bin/dart_lesson_4.dart
+++ bin/dart_lesson_4.dart
@@ -1,18 +1,12 @@
-//Есть коллекция слов. Реализуйте метод, возвращающий Map. Данный Map должен соотносить слово и 
-//количество его вхождений в данную коллекцию.
+//Есть коллекция слов. Реализуйте метод, возвращающий Map. Данный Map должен соотносить слово и количество его вхождений в данную коллекцию.
 void main() {
+  final List<String> NamePerson = ["Alex", "Petr", "Evgen", "Alex"];
+  var a = "Alex";
+  var Rezalt = <String, int>{};
 
-final List <String>NamePerson = ["Alex","Petr","Evgen","Alex"];
-var a = "Alex";
- var Rezalt = <String, int>{};
-
-for(var i = 0; i < 9; i++){
-
-  if(NamePerson.isEmpty == a){
-print('один');
+  for (var i = 0; i < 9; i++) {
+    if (NamePerson.isEmpty == a) {
+      print('один');
+    }
   }
-
-
-}
-
 }
