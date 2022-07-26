e1033d6149d2160dd07c4767379254ffde1fefb2
siblapin <sib.lapin@mail.ru>
2022-07-26 21:25:38 +0700

    lesson 7 final

--- bin/dart_lesson_7.dart
+++ bin/dart_lesson_7.dart
@@ -3,4 +3,35 @@
 //Запрещается использовать методы math. В случае когда значение вернуть невозможно, необходимо бросать исключение с описанием ошибки.
 import 'dart:math';
 
-void main() {}
+void main() {
+  print(25.sqrt(2, 0.0001));
+}
+
+extension on num {
+  num sqrt(int n, num e) {
+    if (n == 0) return 1;
+    if (n < 0) throw ArgumentError();
+    num xk;
+    num xkk = 1;
+    do {
+      xk = xkk;
+      xkk = ((n - 1) * xk + this / xk.pow(n - 1)) / n;
+    } while (abs(xkk - xk) > e);
+    return xkk;
+  }
+
+  num pow(int n) {
+    num result = this;
+    for (var i = 1; i < n; i++) {
+      result *= this;
+    }
+    return result;
+  }
+
+  num abs(num x) {
+    if (x.sign == -1)
+      return -x;
+    else
+      return x;
+  }
+}
